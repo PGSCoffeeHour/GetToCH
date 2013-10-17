0c8226a80ec15c709da4459b51e7cc7b10305832
R. Zach Lamberty <r.zach.lamberty@gmail.com>
2013-10-17 00:34:09 -0500

    Took forever yo

---
+++ CH.py
@@ -0,0 +1,140 @@
+########################################################################
+#                                                                      #
+#   CoffeeHour.py                                                      #
+#       Zach Lamberty                                                  #
+#       10 - 17 - 2013                                                 #
+#                                                                      #
+#       Om nom, my pretties                                            #
+#                                                                      #
+########################################################################
+
+import scipy
+import os
+import time # heh yeah right
+import datetime # what day is it anyway?
+
+import gradSchool as myHell
+import Advisor
+import Advisor.LastMeeting as BrainOverload
+from underGrad import fondMemories, physicsKnowledgeForQ
+import CoffeeHour
+
+
+#-----------------------------------#
+#   We're all in this together      #
+#-----------------------------------#
+
+class GradStudent():
+    """
+    I'll fill in the docstring when I have time (ZL: 8/1/2008)
+    """
+    
+    def __init__( self, subject ):
+        #   Just in case any of this was allocated for anything else
+        del freetime
+        del sleep
+        del socialLife
+        
+        #   Time allocation
+        for impossibleTask in BrainOverload.firstThousand:
+            #time.mktime( somehow = True, ignoreGChat = True )
+            try:
+                time.mktime( somehow = True, ignoreGChat = False )
+            Except TotalFail as ooooooooohshit:
+                print "THEY SEE ME!!!"
+                raise ooooooooohshit
+        
+        #   Friends
+        if time.anyLeft():
+            print "Go back and check, this is probably not true"
+        else:
+            this = underGrad.fondMemories.howWeDoIt()
+            os.partyHard( this, href = http://youtu.be/WccfbPQNMbg?t=17s )
+        
+        while not self.PhD():
+            self.TA()
+            self.RA()
+            self.KeepWorking()
+    
+    
+    def TA( self ):
+        while True:
+            self.Grade()
+    
+    
+    def Grade( self ):
+        
+        from physClass import students, prof
+        
+        while students.lazyStudents:
+            thisBum = students.lazyStudents[0]
+            thisBum.grade = 'F'
+            prof.talkTo()
+            thisBum.grade = 'A'
+            
+            #   super hacky
+            smartNow = students.lazyStudents.pop()
+    
+    
+    def RA( self ):
+        self.headDown = True
+        self.noseToGrindstone = True
+        Advisor.connect()
+        Advisor.email()
+        #   time consuming
+        #waitForEmail()
+        try:
+            Advisor.goTalk()
+        except RuntimeError as whatTheEffDidChrisSay:
+            return 1
+    
+    
+    def KeepWorking():
+        sadFace = netflixQueue.pop()
+    
+    
+    def PhD():
+        return not yet
+
+
+def getToCoffeeHour():
+    
+    with open( advisorsDoor, 'rb' ) as eyeOfSauron:
+        stealthMode = True
+    
+    #   Shouldn't take too long, unless I'm stuck at Wilson again
+    while not inGradLounge:
+        run quickly.py
+    
+    coffeeHour = CoffeeHour()
+    
+    if coffeeHour.hasBagels:
+        I.omnom()
+        I.weight += coffeeHour.bagel
+    
+    if coffeeHour.hasDonuts:
+        I.omnom()
+        I.weight += coffeeHour.donut
+    
+    if coffeeHour.hasNanners:
+        I.omnom()
+        I.weight += coffeeHour.nanner
+        I.leaveOnFloor()
+        lulz += 9001
+        cornell.lawsuits += 1
+    
+    if not coffeeHour.hasCoffee:
+        I.riot()
+    
+    if not I.satsfied():
+        Zach.cries()
+
+
+if __name__ == "__main__":
+    
+    now = datetime.datetime.now()
+    
+    I = GradStudent( 'Physics' )
+    
+    if ( now.isoweekday() == THURSDAY ) and ( now.hour = 15 ):
+        getToCoffeeHour( I )
